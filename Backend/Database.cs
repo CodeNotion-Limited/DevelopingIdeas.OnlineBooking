9fa62f632c3e486be8ae1646b5e5e6db4a00da4a
dbatt26 <dennisbattisti.26@gmail.com>
2022-08-17 17:06:35 +0200

    Open Door button and notifications

--- Backend/Database.cs
+++ Backend/Database.cs
@@ -4,12 +4,12 @@ public static class Database
 {
     public static readonly List<Appointment> CalendarEvents = new()
     {
-        new() {Start = DateTime.Today.AddDays(-2), End = DateTime.Today.AddDays(-2), Text = "SERVER Birthday"},
-        new() {Start = DateTime.Today.AddDays(-11), End = DateTime.Today.AddDays(-10), Text = "SERVER Day off"},
-        new() {Start = DateTime.Today.AddDays(-10), End = DateTime.Today.AddDays(-8), Text = "SERVER Work from home"},
-        new() {Start = DateTime.Today.AddHours(10), End = DateTime.Today.AddHours(12), Text = "SERVER Online meeting"},
-        new() {Start = DateTime.Today.AddHours(10), End = DateTime.Today.AddHours(13), Text = "SERVER Skype call"},
-        new() {Start = DateTime.Today.AddHours(14), End = DateTime.Today.AddHours(14).AddMinutes(30), Text = "SERVER Dentist appointment"},
-        new() {Start = DateTime.Today.AddDays(1), End = DateTime.Today.AddDays(12), Text = "SERVER Vacation"}
+        new() {Start = DateTime.Today.AddDays(-2), End = DateTime.Today.AddDays(-2), Text = "SERVER Birthday", UserEmail = "dennisbattisti.26@gmail.com"},
+        new() {Start = DateTime.Today.AddDays(-11), End = DateTime.Today.AddDays(-10), Text = "SERVER Day off", UserEmail = "dennisbattisti.26@gmail.com"},
+        new() {Start = DateTime.Today.AddDays(-10), End = DateTime.Today.AddDays(-8), Text = "SERVER Work from home", UserEmail = "dennisbattisti.26@gmail.com"},
+        new() {Start = DateTime.Today.AddHours(10), End = DateTime.Today.AddHours(12), Text = "SERVER Online meeting", UserEmail = "dennisbattisti.26@gmail.com"},
+        new() {Start = DateTime.Today.AddHours(10), End = DateTime.Today.AddHours(18), Text = "SERVER Skype call", UserEmail = "dennisbattisti.26@gmail.com"},
+        new() {Start = DateTime.Today.AddHours(14), End = DateTime.Today.AddHours(14).AddMinutes(30), Text = "SERVER Dentist appointment", UserEmail = "dennisbattisti.26@gmail.com"},
+        new() {Start = DateTime.Today.AddDays(1), End = DateTime.Today.AddDays(12), Text = "SERVER Vacation", UserEmail = "dennisbattisti.26@gmail.com"}
     };
 }
--- Frontend/Program.cs
+++ Frontend/Program.cs
@@ -10,6 +10,7 @@ builder.RootComponents.Add<HeadOutlet>("head::after");
 builder.Services.AddScoped(sp => new HttpClient {BaseAddress = new Uri("https://localhost:7205")});
 // builder.Services.AddScoped(sp => new HttpClient {BaseAddress = new Uri("https://blazor-wasm-academy-server.herokuapp.com/")});
 builder.Services.AddTransient<IAppointmentClient, AppointmentClient>();
+builder.Services.AddTransient<IAccessClient, AccessClient>();
 builder.Services.AddScoped<DialogService>();
 builder.Services.AddScoped<NotificationService>();
 
