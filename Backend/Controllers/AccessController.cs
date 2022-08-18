1feb8721fb4f082d6f59be1209f7c5e8851f14f6
Dario <dario.ferrai>
2022-08-18 10:08:31 +0300

    Accessing Nuki API

--- Backend/Controllers/AccessController.cs
+++ Backend/Controllers/AccessController.cs
@@ -1,3 +1,4 @@
+using System.Text;
 using Microsoft.AspNetCore.Mvc;
 
 namespace BlazorWasmAcademyServer.Controllers;
@@ -13,14 +14,21 @@ public class AccessController : ControllerBase
         .Any(x => x.UserEmail == email);
 
     [HttpPut]
-    public bool Access(string email)
+    public async Task<bool> Access(string email)
     {
         if (IsRegistered(email))
         {
-            // TODO: Dario implement Nuki Access
-            return true;
+            return await OpenLockAsync();
         }
 
         return false;
+    }
+
+    private async Task<bool> OpenLockAsync()
+    {
+        var client = new HttpClient();
+        client.DefaultRequestHeaders.Add("Authorization", "Bearer 6vMsGPUamPsu-GZ0Ejod3eLZ52zzD155zdj1_1OCka4.EF18tsVU8P1gkTnxMGjCBq7hNGaS56GyLDaXWEwI5_I");
+        var result = await client.PostAsync("https://api.nuki.io/smartlock/17988037356/action", new StringContent("{\"action\":3,\"option\":0}", Encoding.Default, "application/json"));
+        return result.IsSuccessStatusCode;
     }
 }
