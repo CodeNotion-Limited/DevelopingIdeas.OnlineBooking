781b5ef0ea392ad053d397f251f5a4c5d466de0e
Dario <dario.ferrai>
2022-08-18 10:17:20 +0300

    Adding AccessLog

--- Backend/Controllers/AccessController.cs
+++ Backend/Controllers/AccessController.cs
@@ -29,6 +29,21 @@ public class AccessController : ControllerBase
         var client = new HttpClient();
         client.DefaultRequestHeaders.Add("Authorization", "Bearer 6vMsGPUamPsu-GZ0Ejod3eLZ52zzD155zdj1_1OCka4.EF18tsVU8P1gkTnxMGjCBq7hNGaS56GyLDaXWEwI5_I");
         var result = await client.PostAsync("https://api.nuki.io/smartlock/17988037356/action", new StringContent("{\"action\":3,\"option\":0}", Encoding.Default, "application/json"));
+        
         return result.IsSuccessStatusCode;
     }
+
+    [HttpGet("access-log")]
+    public Task<List<AccessLog>> GetAccesses(string email)
+    {
+        // chiamare in get https://api.nuki.io/smartlock/17988037356/log?limit=20
+
+        // con la stessa logica di sopra
+
+        // per tradurre il risultato in una lista di oggetti c#
+        
+        //var list = await result.Content.ReadFromJsonAsync<AccessLog>();
+    }
 }
+
+// Root myDeserializedClass = JsonConvert.DeserializeObject<List<Root>>(myJsonResponse);
---
+++ Backend/Controllers/AccessLog.cs
@@ -0,0 +1,15 @@
+﻿namespace BlazorWasmAcademyServer.Controllers;
+
+public class AccessLog
+{
+    public string id { get; set; }
+    public object smartlockId { get; set; }
+    public int deviceType { get; set; }
+    public string authId { get; set; }
+    public string name { get; set; }
+    public int action { get; set; }
+    public int trigger { get; set; }
+    public int state { get; set; }
+    public bool autoUnlock { get; set; }
+    public DateTime date { get; set; }
+}
